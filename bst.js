6e938a63121965d2cb6fffcdb5f47896e389ce97
newerk <juniusowens@yahoo.com>
2023-02-22 10:30:35 -0500

    Fix Indentation and Remve Brackets From Classes

--- bst.js
+++ bst.js
@@ -1,28 +1,28 @@
-class Node() {
+class Node {
   contructor(data) {
-  this.data = data;
-  this.left = null;
-  this.right = null;
+    this.data = data;
+    this.left = null;
+    this.right = null;
   }
 }
 
-class Tree() {
+class Tree {
   constructor(array){
-  this.array = array;
-  this.root = buildTree(array, 0, array.length-1);
+    this.array = array;
+    this.root = buildTree(array, 0, array.length - 1);
   }
 }
 
-function buildTree(array, start, end){
-  if(start > end){
-  return null;
+function buildTree(array, start, end) {
+  if (start > end) {
+    return null;
   }
-  
-  let mid = (start+end/2);
+
+  let mid = (start + end / 2);
   let node = new Node(array[mid]);
-  
+
   node.left = buildTree(array, start, mid - 1);
   node.right = buildTree(array, mid + 1, end);
-  
+
   return node;
 }
